b23892b7a076ea3f16497760735d8ab9ff9d3596
Trevor Nodland <trevor.nodland@gmail.com>
2019-03-13 13:32:05 -0600

    updates factories for easier use of comedian stastics

--- spec/factories/comedian.rb
+++ spec/factories/comedian.rb
@@ -2,10 +2,18 @@ FactoryBot.define do
   factory :comedian do
     sequence(:name) {|n| "comedian#{n}"}
     sequence(:city) {|n| "city#{n}"}
-    sequence(:age) {|n| ("#{n}".to_i+30)}
+    age { 60 }
   end
 
   factory :deceased_comedian, parent: :comedian do
     deceased { true }
+  end
+
+  factory :old_comedian, parent: :comedian do
+    age { 90 }
+  end
+
+  factory :young_comedian, parent: :comedian do
+    age { 30 }
   end
 end
--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -3,40 +3,58 @@ require "rails_helper"
 RSpec.describe "comedians index page" do
   context "as a visitor" do
     before :each do
-      @com1 = create(:comedian)
-      @com2 = create(:comedian)
-      @dcom1 = create(:deceased_comedian)
-      @sp1 = create(:special, comedian: @com1)
-      @sp2 = create(:special, comedian: @com1)
+      @com = create(:comedian)
+      @young_com = create(:young_comedian)
+      @old_com = create(:old_comedian)
+      @deceased_com = create(:deceased_comedian)
+
+      @sp1 = create(:special, comedian: @com)
+      @sp2 = create(:special, comedian: @com)
+      @sp3 = create(:special, comedian: @young_com)
     end
 
     it "should see all comedians" do
       visit comedians_path
 
-      within "#comedian-#{@com1.id}" do
-        expect(page).to have_content(@com1.name)
-        expect(page).to have_content(@com1.age)
-        expect(page).to have_content(@com1.city)
-        expect(page).to_not have_content(@com2.age)
+      within "#comedian-#{@com.id}" do
+        expect(page).to have_content(@com.name)
+        expect(page).to have_content(@com.age)
+        expect(page).to have_content(@com.city)
+        expect(page).to_not have_content(@young_com.age)
       end
 
-      within "#comedian-#{@com2.id}" do
-        expect(page).to have_content(@com2.name)
-        expect(page).to have_content(@com2.age)
-        expect(page).to have_content(@com2.city)
-        expect(page).to_not have_content(@com1.age)
+      within "#comedian-#{@young_com.id}" do
+        expect(page).to have_content(@young_com.name)
+        expect(page).to have_content(@young_com.age)
+        expect(page).to have_content(@young_com.city)
+        expect(page).to_not have_content(@com.age)
       end
     end
 
     it "should see all specials for each comedian" do
       visit comedians_path
-      within "#comedian-#{@com1.id}" do
+      within "#comedian-#{@com.id}" do
         within "#specials" do
           expect(page).to have_content(@sp1.name)
           expect(page).to have_content(@sp1.length)
           # expect(page).to have_css("img[src*=#{@sp1.image}]")
           expect(page).to have_content(@sp2.name)
         end
+      end
+
+      within "#comedian-#{@young_com.id}" do
+        within "#specials" do
+          expect(page).to have_content(@sp3.name)
+          expect(page).to have_content(@sp3.length)
+          # expect(page).to have_css("img[src*=#{@sp3.image}]")
+        end
+      end
+    end
+
+    xit "should see some statistics" do
+      visit comedians_path
+      within "#statistics" do
+        expect(page).to have_content("Average age: 30")
       end
     end
   end
